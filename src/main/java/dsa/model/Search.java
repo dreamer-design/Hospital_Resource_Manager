825687f008d0b06f49d0b4f2b37a22d9ae70746f
dreamer-desgn <infiniteinformation@proton.me>
2025-10-21 21:29:41 +0800

    breadth first search untested

--- src/main/java/dsa/model/Search.java
+++ src/main/java/dsa/model/Search.java
@@ -10,7 +10,8 @@ import dsa.structs.Queue;
  */
 public class Search {
     
-    
+    public static LinkedList<Queue> levels = new LinkedList<>(); // each level?
+
     
         /**
      * A* shortest path src, dest
@@ -22,13 +23,6 @@ public class Search {
         return 0;
     }
     
-    /**
-     * helper get max n levels to set size of bfs array.
-     */
-    public int nLevels(Graph g) {
-        return 0;
-    }
-    
         /**
      * Breadth-First Search (BFS): 
      * Input = source department; 
@@ -36,39 +30,64 @@ public class Search {
      * @param src
      * @param dest
      */
-    public static int bfs(Department src) {
-        LinkedList<Queue> levels = new LinkedList<>(); // list of list to hold the levels
-        Queue level = new Queue(); // working memory
+    public static Boolean bfs(Graph g, Department src, Department dest) {        
+        Queue T = new Queue();  // traversed
+        Queue Q = new Queue(); // working memory    // (Q) frontier
         
         // get adjacency list for current corridor
-        LinkedList<Department> current = src.getAdjList(); // 
+        LinkedList<Department> current = src.getAdjList(); //
         
-        src.setVisited(); // set root as visited
-        
-        for( Department d : current) {
-            level.enqueue(d); // add all adjacent nodes to a queue
-            d.setVisited(); // been here
+        // 1. Clear visited flags for all vertices
+//        var verticesList = DataModel.getGraphInstance().getDepartments();  // im going to start using var for references
+        var verticesList = g.getDepartments();  // im going to start using var for references
+        for (Department v : verticesList) {
+            v.setVisited();
         }
         
-        levels.insertLast(level); // add current level
+        // 2. set startng vertice 
+        var start = src;
+        var v = start; // ref pseudo
+        v.setVisited(); // set root as visited
         
-        // recursively call?
+        // enque v into Q
+        Q.enqueue(v);
         
-//        Graph start = DataModel.getGraphInstance(); // do i nee this to chek for visited??
-        return 0;
+        while( !Q.isEmpty() ) {
+            // var set to object   // change once ive go it down
+            v = Q.dequeue(); // dqueue the next node
+            
+            // then go through list an find unvisited
+            // pseudo: for each vertex w in v's adjacency list that is 
+            for( Department w : v.getAdjList() ) {
+                if( !w.getVisited() ) { // unvisited
+                    T.enqueue(w); // add  to traversed
+                    w.setVisited(); // set visited
+                    Q.enqueue(w); // add to working memory
+                }
+                if( w == dest) return true;
+            }
+            levels.insertLast(Q); // should be a level
+        }
+        
+        return false;
     }
     
-//Declare T = DSAQueue and Q = DSAQueue
-//Iterate through your vertices list and clear visited
-//Reference a vertex from your vertices list as v
-//Set v as visited
-//Enqueue v into Q
-//while Q is not empty
-//v = Q.dequeue()
-//for each vertex w in v's adjacency list that is unvisited
-//T.enqueue(v)
-//T.enqueue(w)
-//Set w as visited
-//Enqueue w into Q
-    
+    public static void main(String[] args) {
+        Graph t = new Graph();
+        t.addDepartment(1, "Surgery");
+        t.addDepartment(2, "Ward");
+        t.addDepartment(3, "Waiting");
+        t.addDepartment(4, "Emergency");
+        t.addDepartment(5, "Administration");
+        t.addDepartment(6, "Smoko");
+        t.addCorridore(1, 2);
+        t.addCorridore(2, 3);
+        t.addCorridore(4, 2);
+        t.addCorridore(1, 3);
+        t.addCorridore(6, 1);
+        t.addCorridore(5, 2);
+        t.addCorridore(6, 2);
+        
+        Search.bfs(t, t.getDepartment(1), t.getDepartment(5));
+    }
 }
